3fb30ee7473501b5f2ec827f50cc67d8ff4fc404
anfegoca <anfegoca@gmail.com>
2020-03-23 21:13:47 -0500

    Primer despliegue

--- src/main/java/edu/eci/cvds/samples/services/ServiciosAlquilerFactory.java
+++ src/main/java/edu/eci/cvds/samples/services/ServiciosAlquilerFactory.java
@@ -7,7 +7,6 @@ import edu.eci.cvds.sampleprj.dao.TipoItemDAO;
 import edu.eci.cvds.sampleprj.dao.mybatis.MyBATISClienteDAO;
 import edu.eci.cvds.sampleprj.dao.mybatis.MyBATISItemDAO;
 import edu.eci.cvds.sampleprj.dao.mybatis.MyBATISTipoItemDAO;
-import edu.eci.cvds.samples.services.impl.ServiciosAlquilerImpl;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import org.mybatis.guice.XMLMyBatisModule;
@@ -17,7 +16,8 @@ import java.util.Optional;
 import static com.google.inject.Guice.createInjector;
 import edu.eci.cvds.sampleprj.dao.ItemRentadoDAO;
 import edu.eci.cvds.sampleprj.dao.mybatis.MyBATISItemRentadoDAO;
-//import edu.eci.cvds.samples.services.impl.ServiciosAlquilerItemsStub;
+import edu.eci.cvds.samples.services.impl.ServiciosAlquilerItemsStub;
+
 
 public class ServiciosAlquilerFactory {
 
@@ -35,8 +35,7 @@ public class ServiciosAlquilerFactory {
                bind(ClienteDAO.class).to(MyBATISClienteDAO.class);
                bind(TipoItemDAO.class).to(MyBATISTipoItemDAO.class);
                bind(ItemRentadoDAO.class).to(MyBATISItemRentadoDAO.class);
-               //bind(ServiciosAlquiler.class).to(ServiciosAlquilerItemsStub.class);
-               bind(ServiciosAlquiler.class).to(ServiciosAlquilerImpl.class);
+               bind(ServiciosAlquiler.class).to(ServiciosAlquilerItemsStub.class);
            }
        });
    }
